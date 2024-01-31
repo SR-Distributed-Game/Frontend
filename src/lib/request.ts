5bcdc7c96adfc7262621dc184a608d12cdd2de8f
ronan tremoureux <ronan.tremoureux@gmail.com>
2024-01-31 19:11:08 +0100

    add default room id

--- src/lib/request.ts
+++ src/lib/request.ts
@@ -2,10 +2,13 @@ export class gameRequest {
     Type: string;
     ClientID: number;
     Metadata: any;
+    RoomID : number;
 
     constructor(type: string) {
         this.Type = type;
         this.ClientID = -1;
+        this.RoomID  = -1;
         this.Metadata = {};
     }
+
 }
