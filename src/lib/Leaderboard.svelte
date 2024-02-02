bd4a3d199411eb98c4356d594eb534cf5983bdfe
ronan tremoureux <ronan.tremoureux@gmail.com>
2024-02-02 19:59:52 +0100

    in sync with V1

---
+++ src/lib/Leaderboard.svelte
@@ -0,0 +1,7 @@
+<div class = "flex mt-10">
+    <div class = "m-auto block">
+        <div class = "text-2xl text-white">Leaderboard</div>
+        <div class = "m-auto bg-black bg-opacity-30 p-2 min-w-[50vw] text-white rounded-lg" id=leaderboard>
+        </div>
+    </div>
+</div>
