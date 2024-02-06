aa3079ee3f0a1c3a046b75e4722093e855d36689
ronantremoureux <ronan.tremoureux@etudiant.univ-rennes1.fr>
2024-02-06 17:40:20 +0100

    add proof for serialization

--- src/lib/implementedGames/fruit.ts
+++ src/lib/implementedGames/fruit.ts
@@ -5,12 +5,18 @@ import type p5 from "p5";
 import { GameObject } from "../GameEngine/GameObject";
 
 import { DrawElipseComponent } from "$lib/GameEngine/Components/DrawElipseComponent";
+import { Serializable } from "$lib/GameEngine/Serialized";
 
 export class fruit extends GameObject {
     gfx:DrawRectangleComponent;
     randomColor:string;
+
+    @Serializable
+    public SerializationTest: string;
+
     constructor() {
         super();
+        this.SerializationTest = "";
         this.setName("fruit");
         this.setId((Math.random()*1000));
         this.getTransform().getScale().setX(10);
@@ -27,7 +33,7 @@ export class fruit extends GameObject {
     start(): void {
         this.addDrawComponent(this.gfx);
         this.addComponent( new ColliderComponent(this));
-
+        console.log("fruit started with serialization test: "+this.SerializationTest);
     }
 
     update(p: p5): void {
